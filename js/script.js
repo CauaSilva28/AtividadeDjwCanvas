1d86a26cc0acf29de12540e2fb933057fe03cc75
CauaSilva28 <caua.santos56@etec.sp.gov.br>
2023-05-08 21:02:37 -0300

    Fazendo alterações no html css, e javascript

--- js/script.js
+++ js/script.js
@@ -1,88 +1,58 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-var x = 5;
-var y = 12;
-var m = 30;
-var n = 20;
+const imgbola = new Image();
+imgbola.src = 'img/bola.png';
 
-function papaismurf(){
-    ctx.clearRect(0,0,1000,700);
-    ctx.fillStyle = '#ff0000';
-    ctx.fillRect(x,y,15,-8);
-    ctx.fillStyle = '#b0b0b0';
-    ctx.fillRect(x,y,15,15);
-    ctx.fillStyle = '#009dff';
-    ctx.fillRect(x,y,15,10);
+circulo();
+linha();
+curva();
+curva2();
+retangulo();
+imagem();
+
+function imagem(){
+    ctx.drawImage(imgbola, 480, 330, 40, 40);
+}
+
+function retangulo(){
+    ctx.fillStyle = '#b88461';
+    ctx.fillRect(300,320,40,-20);
+    ctx.fillStyle = '#000';
+    ctx.fillRect(300,320,40,60);
+    ctx.fillStyle = '#fff700';
+    ctx.fillRect(300,320,40,45);
 }
 
 function linha(){
+    ctx.strokeStyle = '#fff';
+    ctx.lineWidth = 4;
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(m, n);
+    ctx.moveTo(500, 0);
+    ctx.lineTo(500, 1000);
     ctx.stroke();
 }
 
 function circulo(){
+    ctx.strokeStyle = '#fff';
+    ctx.lineWidth = 4;
     ctx.beginPath();
-    ctx.arc(50,50,10,0,Math.PI*2,true);
+    ctx.arc(500,350,100,0,Math.PI*2,true);
+    ctx.fillStyle = "#ff9d8a";
+    ctx.fill();
     ctx.stroke();
 }
 
 function curva(){
+    ctx.strokeStyle = '#fff';
     ctx.beginPath();
-    ctx.arc(50,52,5,3,Math.PI/20,true);
+    ctx.arc(0,350,150,Math.PI/2,3*Math.PI/2,true);
     ctx.stroke();
 }
 
-papaismurf();
-linha();
-circulo();
-curva();
-
-document.addEventListener('keydown', function(event){
-    papaismurf();
-    linha();
-    circulo();
-    curva();
-
-    if(event.key === 'd'){
-        x += 5;
-        m += 5;
-    }
-
-    if(event.key === 'a'){
-        x -= 5;
-        m -= 5;
-    }
-
-    if(event.key === 's'){
-        y+=5;
-        n+=5
-    }
-
-    if(event.key === 'w'){
-        y-=5;
-        n-=5;
-    }
-
-    if(x>285){
-        x = 285;
-    }
-
-    if(m>305){
-        m = 299;
-    }
-
-    if(x<0){
-        x = 0;
-    }
-    
-    if(y<8){
-        y = 8;
-    }
-
-    if(y>135){
-        y = 135;
-    }
-});
+function curva2(){
+    ctx.strokeStyle = '#fff';
+    ctx.beginPath();
+    ctx.arc(1000,350,150,Math.PI/2,3*Math.PI/2);
+    ctx.stroke();
+}
